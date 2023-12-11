203ddc4a90ae3b50c76902c268774617774c8cc8
Muhammad Sidiq Hardiansyah <msidiqh991@gmail.com>
2023-12-11 20:33:21 +0700

    modif userModel to gain 2 role

--- CC-API/src/models/userModel.js
+++ CC-API/src/models/userModel.js
@@ -1,5 +1,16 @@
 const mongoose = require('mongoose')
 
+const workerSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true,
+    },
+    dateOfBirth: {
+        type: Date,
+        required: true,
+    }
+})
+
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
@@ -17,12 +28,27 @@ const userSchema = new mongoose.Schema({
     address: {
         type: String,
     },
+    role: {
+        type: String,
+        enum: ['Worker', 'Recruiter'],
+        default: 'Worker',
+    },
+    workerInfo: {
+        type: mongoose.Scema.Types.ObjectId,
+        ref: 'Worker',
+    },
+    RecruiterInfo: {
+        type: mongoose.Scema.Types.ObjectId,
+        ref: 'Recruiter',
+    },
     refreshToken: {
         type: String,
     },
 });
 
+const Worker = mongoose.model('Worker', workerSchema);
+
 const Users = mongoose.model('Users', userSchema);
 
-module.exports = { Users }
+module.exports = { Users, Worker }
 
