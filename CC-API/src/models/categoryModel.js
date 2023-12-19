0ac1e876316138cde8e493ed0834a1e8f5d1b3d7
fadhilaf <fadhilaf3421@gmail.com>
2023-12-19 15:55:24 +0700

    finished creating predict api

--- CC-API/src/models/categoryModel.js
+++ CC-API/src/models/categoryModel.js
@@ -18,37 +18,37 @@ const categoriesSchema = new mongoose.Schema({
 
 const Categories = mongoose.model('Categories', categoriesSchema);
 
+const LIST_CATEGORIES = [
+    {
+        name: 'Cleaning',
+        description: 'Services related to cleaning tasks.',
+        categoriesImage: `https://storage.googleapis.com/${bucketName}/Cleaning.png`
+    },
+    {
+        name: 'Electrical Help',
+        description: 'Services related to electrical issues.',
+        categoriesImage: `https://storage.googleapis.com/${bucketName}/Electrical%20Help.png`
+    },
+    {
+        name: 'Plumbing',
+        description: 'Services related to plumbing tasks.',
+        categoriesImage: `https://storage.googleapis.com/${bucketName}/Plumbing.png`
+    },
+    {
+        name: 'Laundry and Ironing',
+        description: 'Services related to laundry and ironing clothes.',
+        categoriesImage: `https://storage.googleapis.com/${bucketName}/Laundry%20%26%20Ironing.png`
+    },
+    {
+        name: 'Help Moving',
+        description: 'Services related to moving assistance.',
+        categoriesImage: `https://storage.googleapis.com/${bucketName}/Help%20Moving.png`
+    }
+]
+
 const seedCategories = async () => {
     try {
-        const categories = [
-            {
-                name: 'Cleaning',
-                description: 'Services related to cleaning tasks.',
-                categoriesImage: `https://storage.googleapis.com/${bucketName}/Cleaning.png`
-            },
-            {
-                name: 'Electrical Help',
-                description: 'Services related to electrical issues.',
-                categoriesImage: `https://storage.googleapis.com/${bucketName}/Electrical%20Help.png`
-            },
-            {
-                name: 'Plumbing',
-                description: 'Services related to plumbing tasks.',
-                categoriesImage: `https://storage.googleapis.com/${bucketName}/Plumbing.png`
-            },
-            {
-                name: 'Laundry and Ironing',
-                description: 'Services related to laundry and ironing clothes.',
-                categoriesImage: `https://storage.googleapis.com/${bucketName}/Laundry%20%26%20Ironing.png`
-            },
-            {
-                name: 'Help Moving',
-                description: 'Services related to moving assistance.',
-                categoriesImage: `https://storage.googleapis.com/${bucketName}/Help%20Moving.png`
-            }
-        ]
-
-        for(const category of categories) {
+        for(const category of LIST_CATEGORIES) {
             await Categories.findOneAndUpdate(
                 { name: category.name },
                 category,
@@ -61,4 +61,4 @@ const seedCategories = async () => {
     }
 }
 
-module.exports = { Categories, seedCategories }
+module.exports = { Categories, seedCategories, LIST_CATEGORIES}
